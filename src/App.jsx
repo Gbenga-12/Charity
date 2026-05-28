cc8b8ea8a453ce74a4225ce0c3c1756c50f15134
ifeanyidanndy27@gmail.com <ifeanyidanddy27@gmail.com>
2026-05-28 21:02:15 +0100

    corrected an error

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Home from "./Pages/Home";
-import Volunteer from "./Pages/Volunteer";
+// import Home from "./Pages/Home";
+// import Volunteer from "./Pages/Volunteer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Fundraiser from "./Pages/Fundraiser";
+// import Fundraiser from "./Pages/Fundraiser";
 import Partners from "./Pages/Partners";
 import Contact from "./Pages/Contact";
 import Services from "./Pages/Services";
 import Volunteer from "./Pages/Volunteer";
 import Fundraiser from "./Pages/Fundraiser";
 import Home from "./Pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Education from "./Pages/Education";
 import Health from "./Pages/Health";
 // import Services from "./components/Services";
@@ -20,7 +20,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/volunteer" element={<Volunteer />} />
-        <Route path="/fundraiser" element={<Fundraiser />} />
+        {/* <Route path="/fundraiser" element={<Fundraiser />} /> */}
         <Route path="/partners" element={<Partners />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
